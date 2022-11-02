b23d4e3b5326f5246f6921dbdf4405627b1918b5
Max Wank <maxwank11@gmail.com>
2022-11-02 12:38:27 -0400

    finished

--- src/bubbleSort.java
+++ src/bubbleSort.java
@@ -1,37 +1,42 @@
 public class bubbleSort {
 
         public static void main(String[] args) {
-            //create array   0  1    2  3  4  5  6   7   8   9
-            int[] elements = {1, 661, 8, 2, 4, 1, 22, 66, 20, 122};
+            //create array   0  1    2  3  4  5  6   7   8
+            int[] elements = {1, 661, 8, 2, 4556, 4, 22, 66, 20, 122, 65, 1224, 234, 123, 436};
             //print the unsorted array
             clear();
-            for (int i = 0; i <= 9; i++) {
-                System.out.println();
-                System.out.print(elements[i]+"|");
+            System.out.println("Entered Values:");
+            for (int i = 0; i <= 14; i++) {
+                System.out.print(elements[i] + "|");
             }
+            wait(1000);
             System.out.println();
-            for (int i=0; i<=8; i++){
-                for (int j=0; j<=8; j++){
-                    if (elements[j]>elements[j+1]){
-                        int temp=elements[j];
-                        elements[j]=elements[j+1];
-                        elements[j+1]=temp;
+            System.out.println("Sorting...");
+            wait(1000);
+            System.out.println();
+            for (int i = 0; i <= 14; i ++ ) {
+                for (int j = 0; j <= 13; j++ ) {
+                    if (elements[j] > elements[j + 1]) {
+                        int temp = elements[j];
+                        elements[j] = elements[j + 1];
+                        elements[j + 1] = temp;
                     }
                 }
-                for (int l = 0; l <= 9; l++) {
-                    System.out.println();
-                    System.out.print(elements[i]+"|");
+                for (int k = 0; k <= 14; k++ ) {
+                    System.out.print(elements[k] + "|");
                 }
                 System.out.println();
-                wait(100);
+                wait(1000);
+            }
+            clear();
+            System.out.println("Done!");
+            wait(500);
+            for (int j = 0; j <= 14; j ++ ) {
+                System.out.print(elements[j]+"|");
             }
             wait(1000);
             clear();
-            System.out.print("Done!");
-            for (int i = 0; i <= 9; i++) {
-                System.out.println();
-                System.out.print(elements[i]+"|");
-            }
+            wait(400);
         }
 
     public static void clear() {
